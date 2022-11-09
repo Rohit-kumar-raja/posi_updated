1e3b305e6de6f7752f72c8af394f976ac6e75352
Raja <raja@raja.com>
2022-11-09 20:50:52 +0530

    all doen

--- database/getMyImagePost.php
+++ database/getMyImagePost.php
@@ -3,17 +3,20 @@ include("connection.php");
 
 // mysqli_select_db($conn,"posigraph_socialplexus");
 function image($userId)
-{ 
-    global $conn;
-    // echo "select * from  posts where userId={$_SESSION['id']}  AND type LIKE 'image%' ORDER BY post#pop-up-divDate DESC";
-    // exit;
-    $query="select * from  posts where userId={$userId}  AND type LIKE 'image%' ORDER BY postDate DESC";
-    $image=mysqli_query($conn,$query);
-    while($row=mysqli_fetch_array($image))
-    {
-      $img_id =  $row['postId'] ;
-     echo "<img src='imagePost/{$row['postImage']}' class='{$row['postId']}'>";
-    }
+{
+  global $conn;
+  // echo "select * from  posts where userId={$_SESSION['id']}  AND type LIKE 'image%' ORDER BY post#pop-up-divDate DESC";
+  // exit;
+  $query = "select * from  posts where userId={$userId}  AND type LIKE 'image%' ORDER BY postDate DESC";
+  $image = mysqli_query($conn, $query);
+  while ($row = mysqli_fetch_array($image)) {
+    $img_id =  $row['postId'];
+?>
+          <a class="centered" href=""> <i class="fas fa-trash-alt"></i></a>
+      <img src='imagePost/<?= $row['postImage'] ?>' class='<?= $row['postId'] ?>'>
+  
+<?php
+  }
 }
 
 
