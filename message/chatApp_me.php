12fcfcbef0305a46edd9789acca5e1cfacc3e034
ajit9639 <61927091+ajit9639@users.noreply.github.com>
2022-12-06 12:49:00 +0530

    chat msg

--- message/chatApp_me.php
+++ message/chatApp_me.php
@@ -33,7 +33,7 @@ $user=mysqli_fetch_array($user);
         margin-top: 20px;
         /* margin-left: 15px; */
         padding: 20px;
-        box-shadow: -50px 5px 100px 10px #ccc;       
+        /* box-shadow: -50px 5px 100px 10px #ccc;        */
     }
 
     .chatlogs {
