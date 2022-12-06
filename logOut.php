3ade7e12134f9f8c0279b3276e11b8c0671f98d7
ajit9639 <61927091+ajit9639@users.noreply.github.com>
2022-12-07 00:08:26 +0530

    logout

--- logOut.php
+++ logOut.php
@@ -1,15 +1,14 @@
 <?php
 session_start();
 include "database/connection.php";
-session_destroy();
-header("location:index.php");
+$database = "posigraph_socialplexus";
 
-// $upd = "update user set logInStatus='Offline' where userId=".$_SESSION['id'];
-// if(mysqli_query($conn,$upd))
-// {
-//      session_destroy();
-//     header("location:index.php");
-// }
-// else
-//     echo mysqli_error($conn);
+$upd = "update user set logInStatus='Offline' where userId=".$_SESSION['id'];
+if(mysqli_query($conn,$upd))
+{
+     session_destroy();
+    header("location:index.php");
+}
+else
+    echo mysqli_error($conn);
 ?>
